c0d2f19e86aa29662470a29e56aebc7b4a9c1bd3
Alexey Fedorov <waterlink000@gmail.com>
2015-01-21 13:47:43 +0100

    additional fix for ruby 1.8

--- spec/restricted_struct_spec.rb
+++ spec/restricted_struct_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe RestrictedStruct do
   it "validates access_level" do
-    expect { described_class.new(:invalid, :some, :properties) }
-      .to raise_error(ArgumentError)
+    expect {
+      described_class.new(:invalid, :some, :properties)
+    }.to raise_error(ArgumentError)
   end
 
   described_class::ACCESS_LEVELS.each do |access_level|
@@ -9,13 +10,15 @@ RSpec.describe RestrictedStruct do
       subject { described_class.new(access_level, :some, :properties) }
 
       it "makes readers #{access_level}" do
-        expect { subject.new("some", "value").some }
-          .to raise_error(NoMethodError, /#{access_level} method `some'/)
+        expect {
+          subject.new("some", "value").some
+        }.to raise_error(NoMethodError, /#{access_level} method `some'/)
       end
 
       it "makes writers #{access_level}" do
-        expect { subject.new("some", "value").properties = 42 }
-          .to raise_error(NoMethodError, /#{access_level} method `properties='/)
+        expect {
+          subject.new("some", "value").properties = 42
+        }.to raise_error(NoMethodError, /#{access_level} method `properties='/)
       end
     end
 
